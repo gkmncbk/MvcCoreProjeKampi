1cd2b3d03fab1ee1ef8166cf206e6fe398b7790c
Gökmen ÇABUK <gokmencabuk@hotmail.com>
2021-11-28 00:41:55 +0300

    v10.2

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.v3 = c.Categories.Count().ToString();
             return View();
         }
     }
